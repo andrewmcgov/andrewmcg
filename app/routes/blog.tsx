3d159246f0e90767673e82285d6132625a960b80
Andrew McGoveran <andrewmcgoveran@gmail.com>
2023-09-26 20:57:45 -0400

    Test loading a post in a loader from a file

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,7 +1,19 @@
-import {Outlet} from '@remix-run/react';
+import {Outlet, useLoaderData} from '@remix-run/react';
 import {Layout} from '~/components/Layout';
+import {getPost} from '~/server/blog.server';
+
+export async function loader() {
+  const post = await getPost();
+
+  console.log('loader running');
+
+  return {post};
+}
 
 export default function Blog() {
+  const data = useLoaderData();
+
+  console.log('post', data);
   return (
     <Layout>
       <div className="mx-auto my-10 mdx-content mdx-content-page">
