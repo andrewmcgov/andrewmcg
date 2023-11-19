9bef19ecff69779d7c25f0f90b32fd7915d0001a
Andrew McGoveran <andrewmcgoveran@gmail.com>
2023-11-18 19:28:10 -0500

    Add generative hero circles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,45 +1,70 @@
-import {Link, useLoaderData} from '@remix-run/react';
 import {Layout} from '~/components/Layout';
+import {GenerativeCanvas} from '~/components/GenerativeCanvas';
 
 export default function Index() {
   return (
     <Layout home>
-      <section className="max-w-3xl my-14 px-4 mx-auto md:my-56 flex flex-col md:flex-row justify-center md:justify-around items-center gap-16">
-        <div className="h-40 w-40 bg-cyan-700 rounded-full"></div>
-        <span className="md:w-1/3 text-cyan-50 font-bold text-2xl">
+      <section className="max-w-3xl my-14 px-4 mx-auto md:my-28 flex flex-col md:flex-row justify-center md:justify-around items-center gap-16">
+        <GenerativeCanvas />
+        <span className="md:w-1/2 text-cyan-50 font-bold text-2xl">
           Hi! I'm Andrew. I'm a software developer who loves music, sports, and
           building on the web.
         </span>
       </section>
-      <section className="bg-cyan-800 rounded-rounded-section md:rounded-md-rounded-section py-20 md:mt-56 ">
+      <section className="bg-cyan-800 rounded-rounded-section md:rounded-md-rounded-section py-20 md:mt-32 ">
         <div className="max-w-3xl px-4 mx-auto leading-relaxed text-lg">
-          <h2 className="text-cyan-50 font-bold text-3xl my-5 underline decoration-slate-800">
+          <h2 className="text-cyan-50 font-bold text-3xl my-8 pt-6 underline decoration-slate-800">
             About me
           </h2>
           <p className="mb-3">
-            I am a web developer from Ottawa, Ontario. I work at{' '}
-            <a href="https://www.shopify.ca/">Shopify</a> where I help build the
-            admin interfaces that millions of entrepreneurs use to build their
-            business.
+            I am a web developer from Ottawa, Ontario. I have worked at{' '}
+            <a className="underline" href="https://www.shopify.ca/">
+              Shopify
+            </a>{' '}
+            for six years, helping build the admin interfaces that millions of
+            entrepreneurs use to build their business. I have had the chance to
+            work in a variety of areas, ranging from online store themes, to
+            inventory management, and now subscriptions.
           </p>
           <p className="mb-3">
-            I primarily work with React and Typescript, and I am working on
-            getting more familiar with Ruby on Rails. You can find me over on{' '}
-            <a href="https://github.com/andrewmcgov">Github</a> where I am
-            currently working on{' '}
-            <a href="https://github.com/andrewmcgov/andrewmcg">this site</a> and
-            a{' '}
-            <a href="https://github.com/andrewmcgov/react-component-generator">
+            Lately I've been working with React, Typescript, and Remix. You can
+            find me over on{' '}
+            <a className="underline" href="https://github.com/andrewmcgov">
+              Github
+            </a>{' '}
+            where I am currently working on{' '}
+            <a
+              className="underline"
+              href="https://github.com/andrewmcgov/andrewmcg"
+            >
+              this site
+            </a>{' '}
+            and a{' '}
+            <a
+              className="underline"
+              href="https://github.com/andrewmcgov/react-component-generator"
+            >
               React component generator for VS Code.
             </a>
           </p>
           <p className="mb-3">
-            Aside from web development, you can find me rooting for the Toronto
-            Maple Leafs, cycling, or playing guitar.
+            Aside from programming, I love playing and listening to music. I
+            actually studied music in university, and have recently been getting
+            back into playing guitar. It's a great way to get into a creative
+            flow state.
           </p>
-          <p>
+          <p className="mb-3">
+            I am a big sports fan, and have had the misfortune of rooting for
+            the Toronto Maple Leafs my whole life. Maybe this is the year? I
+            also love baseball, riding my bike around Ottawa, and visiting local
+            coffee shops.
+          </p>
+          <p className="mb-3">
             Feel free to say hi over on{' '}
-            <a href="https://twitter.com/andrew_mcgov">Twitter</a>!
+            <a className="underline" href="https://twitter.com/andrew_mcgov">
+              Twitter
+            </a>
+            !
           </p>
         </div>
       </section>
