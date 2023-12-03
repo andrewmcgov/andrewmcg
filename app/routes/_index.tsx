f8664bc3e3b984ed4484c738734168dec02acb2d
Andrew McGoveran <andrewmcgoveran@gmail.com>
2023-12-02 21:45:22 -0500

    Add post dates

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,11 +13,11 @@ export default function Index() {
       </section>
       <section className="bg-cyan-800 rounded-rounded-section md:rounded-md-rounded-section py-20 md:mt-32 ">
         <div className="max-w-3xl px-4 mx-auto leading-relaxed text-lg">
-          <h2 className="text-cyan-50 font-bold text-3xl my-8 pt-6 underline decoration-slate-800">
+          <h2 className="text-cyan-50 font-bold text-3xl my-8 pt-6">
             About me
           </h2>
           <p className="mb-3">
-            I am a web developer from Ottawa, Ontario. I have worked at{' '}
+            I am a software developer from Ottawa, Ontario. I have worked at{' '}
             <a className="underline" href="https://www.shopify.ca/">
               Shopify
             </a>{' '}
@@ -27,35 +27,14 @@ export default function Index() {
             inventory management, and now subscriptions.
           </p>
           <p className="mb-3">
-            Lately I've been working with React, Typescript, and Remix. You can
-            find me over on{' '}
-            <a className="underline" href="https://github.com/andrewmcgov">
-              Github
-            </a>{' '}
-            where I am currently working on{' '}
-            <a
-              className="underline"
-              href="https://github.com/andrewmcgov/andrewmcg"
-            >
-              this site
-            </a>{' '}
-            and a{' '}
-            <a
-              className="underline"
-              href="https://github.com/andrewmcgov/react-component-generator"
-            >
-              React component generator for VS Code.
-            </a>
-          </p>
-          <p className="mb-3">
             Aside from programming, I love playing and listening to music. I
-            actually studied music in university, and have recently been getting
-            back into playing guitar. It's a great way to get into a creative
-            flow state.
+            studied music in university, and have recently been getting back
+            into playing guitar. It's a great way to get into a creative flow
+            state.
           </p>
           <p className="mb-3">
             I am a big sports fan, and have had the misfortune of rooting for
-            the Toronto Maple Leafs my whole life. Maybe this is the year? I
+            the Toronto Maple Leafs for my whole life. Maybe this is the year? I
             also love baseball, riding my bike around Ottawa, and visiting local
             coffee shops.
           </p>
